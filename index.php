c7309beb9bdb83f77eabefd972b0de8b0d198d3b
blank <omarshebani2004@gmail.com>
2023-11-04 02:11:06 +0800

    Small adjustments

--- index.php
+++ index.php
@@ -26,20 +26,20 @@
                     <h1>TodoList</h1>
                 </a>
                 <div class="button-container"> 
-                <button class="headerButton" onClick="window.location.href=\'edit-account.php\';">Edit Profile</button>
-                <button class="headerButton" onClick="window.location.href=\'logout.php\';">Logout</button>
+                    <button class="headerButton" onClick="window.location.href=\'edit-account.php\';">Edit Profile</button>
+                    <button class="headerButton" onClick="window.location.href=\'logout.php\';">Logout</button>
                 </div>
-                </header>
+            </header>
 
             <div class="container">
                 <div class="content-container">
                     <h2>Your TodoList<h2>
-                            <h4>This character "¬" can not be entered:<h4>
-                                    <form action="list-manager.php" method="post">
-                                        <input class="inputField" type="text" name="newEntry" placeholder="Entry" maxlength="80"
-                                            pattern="[^¬]*" required title="* Please enter a valid entry">
-                                        <input class="button" type="submit" name="addEntry" value="Add">
-                                    </form>
+                    <h4>This character "¬" can not be entered:<h4>
+                        <form action="list-manager.php" method="post">
+                            <input class="inputField" type="text" name="newEntry" placeholder="Entry" maxlength="80"
+                            pattern="[^¬]*" required title="* Please enter a valid entry">
+                            <input class="button" type="submit" name="addEntry" value="Add">
+                        </form>
                 </div>
             </div>
         
@@ -55,10 +55,7 @@
                     </thead>
             
                     <tbody>
-                    </tbody>
-                </table>
-            </div>
-             ';
+                    ';
 
             $stmt = $conn->prepare("SELECT list FROM users_lists WHERE user_id = ?");
             $stmt->bind_param("i", $_SESSION['user-id']);
@@ -66,45 +63,44 @@
             $stmt->bind_result($currentEntries);
             $stmt->fetch();
             $stmt->close();
+        ?>
 
-            ?>
     <script>
-    function switch_visibility(button) {
-        let entry = button.parentElement.previousElementSibling.querySelector('.visible');
-        let editField = button.parentElement.previousElementSibling.querySelector('.hidden');
-        let editButton = button.parentElement.querySelector('.visible');
-        let doneButton = button.parentElement.querySelector('.hidden');
+        function switch_visibility(button) {
+            let entry = button.parentElement.previousElementSibling.querySelector('.visible');
+            let editField = button.parentElement.previousElementSibling.querySelector('.hidden');
+            let editButton = button.parentElement.querySelector('.visible');
+            let doneButton = button.parentElement.querySelector('.hidden');
 
-        editField.removeAttribute('hidden');
-        editButton.setAttribute('hidden', 'hidden');
-        doneButton.removeAttribute('hidden');
-        entry.setAttribute('hidden', 'hidden');
-    }
+            editField.removeAttribute('hidden');
+            editButton.setAttribute('hidden', 'hidden');
+            doneButton.removeAttribute('hidden');
+            entry.setAttribute('hidden', 'hidden');
+        }
     </script>
+
     <?php
+            if ($currentEntries) {
+                $entryNum = 1;
+                foreach (explode("¬", $currentEntries) as $entry) {
+                    echo '<tr>';
 
-                    if ($currentEntries) {
-                        $entryNum = 1;
-                        foreach (explode("¬", $currentEntries) as $entry) {
-                            echo '<tr>';
+                    echo '<td> <div class="visible">'. $entryNum .' - '. $entry .'</div>
+                  <input class="hidden inputField" type="text" name="editField" placeholder="Edit entry" value="'. $entry .'" maxlength="80" hidden> </td>';
 
-                            echo '<td> <div class="visible">'. $entryNum .' - '. $entry .'</div>
-                          <input class="hidden inputField" type="text" name="editField" placeholder="Edit entry" value="'. $entry .'" maxlength="80" hidden> </td>';
+                    echo '<td> <button class="visible" onClick="switch_visibility(this);">Edit</button>
+                  <button class="hidden" hidden onClick="window.location.href=\'list-manager.php?editedEntry=\' + encodeURIComponent(this.parentElement.previousElementSibling.querySelector(\'.inputField\').value) + \'&entryNum='. $entryNum .'\';">
+                  Done</button> </td>';
 
-                            echo '<td> <button class="visible" onClick="switch_visibility(this);">Edit</button>
-                          <button class="hidden" hidden onClick="window.location.href=\'list-manager.php?editedEntry=\' + encodeURIComponent(this.parentElement.previousElementSibling.querySelector(\'.inputField\').value) + \'&entryNum='. $entryNum .'\';">
-                          Done</button> </td>';
+                    echo '<td> <a href="list-manager.php?delEntryNum=' . $entryNum . '">Delete</a> </td>';
 
-                            echo '<td> <a href="list-manager.php?delEntryNum=' . $entryNum . '">Delete</a> </td>';
+                    echo '</tr>';
+                    $entryNum += 1;
+                }
+            }
 
-                            echo '</tr>';
-                            $entryNum += 1;
-                        }
-                    }
-
-                    echo '</tbody>';
+            echo '</tbody>';
             echo '</table>';
-            echo '</div>';
             echo '</div>';
 
         } else {
